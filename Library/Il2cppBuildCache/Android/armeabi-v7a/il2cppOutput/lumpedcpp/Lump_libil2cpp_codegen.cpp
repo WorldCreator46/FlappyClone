7241a1cedb618f8523cc3e43ac9423fe968bcaed
WorldCreator46 <96541154+WorldCreator46@users.noreply.github.com>
2022-02-19 00:08:44 +0900

    Library
    이게 왜 하루 동안 해결이 안되나

--- Library/Il2cppBuildCache/Android/armeabi-v7a/il2cppOutput/lumpedcpp/Lump_libil2cpp_codegen.cpp
+++
@@ -1,4 +0,0 @@
-#include "il2cpp-config.h"
-#include "E:\Unity\Hub\Editor\2020.3.25f1\Editor\Data\il2cpp\libil2cpp\codegen\il2cpp-codegen-common.cpp"
-#include "E:\Unity\Hub\Editor\2020.3.25f1\Editor\Data\il2cpp\libil2cpp\codegen\il2cpp-codegen-il2cpp.cpp"
-#include "E:\Unity\Hub\Editor\2020.3.25f1\Editor\Data\il2cpp\libil2cpp\codegen\il2cpp-codegen-tiny.cpp"
